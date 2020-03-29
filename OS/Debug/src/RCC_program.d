0e2e66ad654666b09de610eecaf8ba1e9ccd7f22
Omar Sameh Mahmoud <62811657+OmarSamehMahmoud@users.noreply.github.com>
2020-03-30 00:19:13 +0200

    OS Simple Schedular
    OS Simple Schedular

---
+++ OS/Debug/src/RCC_program.d
@@ -0,0 +1,13 @@
+src/RCC_program.o: ../src/RCC_program.c ../include/STD_Types.h \
+ ../include/Bit_Math.h ../include/RCC_int.h ../include/RCC_config.h \
+ ../include/RCC_registers.h
+
+../include/STD_Types.h:
+
+../include/Bit_Math.h:
+
+../include/RCC_int.h:
+
+../include/RCC_config.h:
+
+../include/RCC_registers.h:
---
+++ OS/include/DIO_reg.h
@@ -0,0 +1,20 @@
+#ifndef _DIO_REGISTERS_H
+#define _DIO_REGISTERS_H
+
+typedef struct
+{
+		u32 CRL;
+		u32 CRH;
+		u32 IDR;
+		u32 ODR;
+		u32 BSRR;
+		u32 BRR;
+		u32 LCKR;
+		
+}GPIO;
+
+#define GPIOA ((GPIO*)0x40010800)
+#define GPIOB ((GPIO*)0x40010C00)
+#define GPIOC ((GPIO*)0x40011000)
+
+#endif
